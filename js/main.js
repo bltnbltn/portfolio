fd6ec63b9fbafc5b066ac12f0bde1eae9c2ca155
Yura Lee <64767219+yuralees@users.noreply.github.com>
2022-08-25 16:54:44 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const toggleBtn = document.querySelector('.navbar_toggleBtn');
 const menu = document.querySelector('.nav_menu');
 const navbar = document.querySelector('.navbar');
+const nav_menu = document.querySelectorAll('.nav_menu li');
 
 toggleBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -10,32 +11,6 @@ toggleBtn.addEventListener('click', (e) => {
   } else {
     navbar.style.padding = '14px 0px';
   }
-});
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var images = document.getElementsByClassName('myLogos');
-var modalImg = document.getElementById('img01');
-var captionText = document.getElementById('caption');
-
-// Go through all of the images with our custom class
-for (var i = 0; i < images.length; i++) {
-  var img = images[i];
-  // and attach our click listener for this image.
-  img.onclick = function (evt) {
-    modal.style.display = 'block';
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  };
-}
-
-// Get the <span> element that closes the modal
-
-modal.addEventListener('click', () => {
-  modal.style.display = 'none';
 });
 
 //free load
@@ -58,3 +33,13 @@ preload(
   '../img/art/pho.jpg',
   '../img/art/merry.jpg'
 );
+
+function clickSingleA(a) {
+  items = document.querySelectorAll('.single.active');
+
+  if (items.length) {
+    items[0].className = 'single';
+  }
+
+  a.className = 'single active';
+}
